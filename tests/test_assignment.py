84c1b933d79223e5c53ac8350b69366bedc20db3
Jason Madar <env3d@users.noreply.github.com>
2024-07-11 00:48:41 +0000

    updating tests

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -5,36 +5,55 @@ import re
 
 @pytest.fixture
 def my_server_name():
-    os.popen('cp /usr/lib/cgi-bin/security-base64-encoding/qrcode.sh .').read()
-    server_name = os.popen("sudo apachectl -S | grep -o -P '\\S+wmdd4950.com'").read()
-    if not pathlib.Path('server_name.txt').is_file():
-        f = open('server_name.txt','w')
-        f.write(server_name)
-        f.close()
+    return 'localhost'
 
-    return open('server_name.txt').read().strip()
-
-def test_qr_code_hashbang(my_server_name):
+@pytest.fixture
+def qrcode_sh_content():
     with open('qrcode.sh') as f:
-        content = f.read()
-        assert '#!/bin/bash' in content, "Must be an executable secript"
+        return f.read()        
+
+def test_qr_code_executable():
+    permissions = os.popen('stat -c %A qrcode.sh').read().strip()
+    assert permissions.endswith('x'), "Must be executable with permission 755"
+
+def test_qr_code_hashbang(my_server_name, qrcode_sh_content):
+    assert '#!/bin/bash' in qrcode_sh_content, "Missing hashbang line"
         
-def test_qr_code_content_type(my_server_name):
-    with open('qrcode.sh') as f:
-        content = f.read()
-        assert re.search(r'content-type.*text/html', content, re.IGNORECASE), (
-            "Must have the correct content-type" )
+def test_qr_code_content_type(my_server_name, qrcode_sh_content):
+    assert re.search(r'content-type.*text/html', qrcode_sh_content, re.IGNORECASE), (
+        "Must have the correct content-type" )
+
+def test_qrencode_usage(my_server_name, qrcode_sh_content):
+    assert re.search(r'qrencode', qrcode_sh_content), (
+        "Must use qrencode command" )
+
+def test_base64_usage(my_server_name, qrcode_sh_content):
+    assert re.search(r'base64', qrcode_sh_content), (
+        "Must use qrencode command" )
 
 @pytest.fixture
 def qrcontent(my_server_name):
     content = os.popen(
-        f'curl -s https://{my_server_name}/cgi-bin/security-base64-encoding/qrcode.sh?bye'
+        f'curl -s {my_server_name}/cgi-bin/qrcode.sh?bye'
     ).read()
     return content
     
-def test_qr_code_execute(my_server_name, qrcontent):    
+def test_qr_code_execute1(my_server_name, qrcontent):    
     assert 'data:image/png;base64' in qrcontent
 
-def test_qr_code_execute(my_server_name, qrcontent):
+def test_qr_code_execute2(my_server_name, qrcontent):
     assert 'iVBORw0KGgoAAAANSUhEUgAAAFcAAABXAQMAAABLBksvAAAABlBMVEUAAAD' in qrcontent
+
+def test_qr_code_execute3(my_server_name, qrcontent):
+    assert 'Tffp2Y1nYMbF6weprULZQpneOvv8TS97' in qrcontent
+
+def test_qr_code_execute4(my_server_name):
+    content1 = os.popen(
+        f'curl -s {my_server_name}/cgi-bin/qrcode.sh?abc'
+    ).read()
+    content2 = os.popen(
+        f'curl -s {my_server_name}/cgi-bin/qrcode.sh?123'
+    ).read()
+    assert content1 != content2, "Script seems to be producing the same output for the different input"
+
     
