bdae6d4074da285f30c3c481e8a3bf9ab4cecdd8
Jason Madar <env3d@users.noreply.github.com>
2025-02-13 19:45:04 +0000

    submit

--- qrcode.sh
+++ qrcode.sh
@@ -1,1 +1,23 @@
-# Replace content of this file with your solution
+#!/bin/bash
+
+echo 'content-type: text/html'
+echo 
+
+QUERY_STRING=$1
+
+if [ -z "$QUERY_STRING" ]; then
+  echo "<html><head><title>Error</title></head><body>"
+  echo "<h1>Error: No string provided!</h1>"
+  echo "<p>You are supposed to add a string that you want to encode after cgi-bin/qrcode.sh</p>"
+  echo "</body></html>"
+  exit 1
+fi
+
+ENCODED_QR=$(qrencode -o - "$QUERY_STRING" | base64 -w 0)
+
+echo "<html>"
+echo "<head><title>QR Code</title></head>"
+echo "<body>"
+echo "<img src=\"data:image/png;base64,$ENCODED_QR\" />"
+echo "</body>"
+echo "</html>"
